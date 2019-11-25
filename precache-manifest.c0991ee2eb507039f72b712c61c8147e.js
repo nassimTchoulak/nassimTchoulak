4baef0d708da104670671e532461977e8a650752
Tchoulak <44297966+nassimTchoulak@users.noreply.github.com>
2019-11-25 11:32:36 +0100

    hello world

---
+++ precache-manifest.c0991ee2eb507039f72b712c61c8147e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd65c30b8f2a9c1b3d2675c7bdce65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fef9724718dc0f10d1b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "10e08e83e2ea9dbdd1a2",
+    "url": "/static/js/2.fc033fae.chunk.js"
+  },
+  {
+    "revision": "6fef9724718dc0f10d1b",
+    "url": "/static/js/main.e38c807f.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
